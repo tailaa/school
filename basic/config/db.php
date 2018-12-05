a31f476d5bb0a59d0a29aac3b9b70457cae9e34a
Duscicat <duscicat@gmail.com>
2018-12-05 22:20:30 +0700

    add contactsOfchildrens table

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii',
+    'dsn' => 'mysql:host=localhost;dbname=yii2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',

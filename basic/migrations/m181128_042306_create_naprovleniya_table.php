186e1d454338b9f2533378c14f26b4530a64f1eb
Kollinar1 <kollinar@ya.ru>
2018-12-05 11:00:04 +0700

    Tanaev3

--- basic/migrations/m181128_042306_create_naprovleniya_table.php
+++ basic/migrations/m181128_042306_create_naprovleniya_table.php
@@ -13,9 +13,9 @@ class m181128_042306_create_naprovleniya_table extends Migration
     public function safeUp()
     {
         $this->createTable('naprovleniya', [
-            'id' => $this->primaryKey(),
+            'id_naprovleniya' => $this->primaryKey(),
             'nazvanie' => $this->string()->notNull(),
-            'id_ruk' => $this->int()->notNull(),
+            'id_ruk' => $this->integer()->notNull(),
         ]);
     }
 

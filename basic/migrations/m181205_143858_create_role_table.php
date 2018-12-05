2cfaf35480c109a48cb74c46bd7422c01281eb6d
Duscicat <duscicat@gmail.com>
2018-12-05 21:41:31 +0700

    create role table again

---
+++ basic/migrations/m181205_143858_create_role_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `role`.
+ */
+class m181205_143858_create_role_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('role', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('role');
+    }
+}

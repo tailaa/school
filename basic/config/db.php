f64b15af7ed0fdbb26feaf4f388865c8c410ca00
Tsikhonya Nikita <tsikhonya98@gmail.com>
2018-12-12 10:01:49 +0700

    tsikhonya commit

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii_base',
+    'dsn' => 'mysql:host=localhost;dbname=yii2base',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',

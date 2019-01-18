930bbd6dcd095b1ebe34ce7ca3d1d585a6120fb5
Duscicat <duscicat@gmail.com>
2019-01-18 11:07:15 +0700

    change db file

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=trans',
+    'dsn' => 'mysql:host=localhost;dbname=git',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
